a1156574fc0a0b5a374790c7dca60bc45de62cbd
RuslanMirasov <info@mirasov.dev>
2023-04-01 23:53:27 +0200

    popup settings

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,9 @@
     openModalBtn: document.querySelectorAll("[data-modal-open]"),
     closeModalBtn: document.querySelectorAll("[data-modal-close]"),
     modal: document.querySelectorAll("[data-modal]"),
+    modalPaddingElements: [].filter.call(document.all, e => getComputedStyle(e).position == 'fixed'),
+    body: document.querySelector(".body"),
+    bodyPadding: window.innerWidth - document.querySelector('.main').offsetWidth
   };
 
   refs.openModalBtn.forEach(modalBtn => {
@@ -16,6 +19,20 @@
   function toggleModal() {
     refs.modal.forEach(modalClass => {
       modalClass.classList.toggle("is-hidden");
+    });
+    refs.body.classList.toggle("lock");
+    bodyPaddingToggle();
+  }
+  function bodyPaddingToggle() {
+    refs.modalPaddingElements.forEach(modalPaddingElements => {
+      if (refs.body.classList.contains('lock')) {
+        refs.body.style.paddingRight = refs.bodyPadding + 'px';
+        modalPaddingElements.style.paddingRight = refs.bodyPadding + 'px';
+      }
+      else {
+        refs.body.style.paddingRight = '0px';
+        modalPaddingElements.style.paddingRight = '0px';
+      }
     });
   }
 })();
